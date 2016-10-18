ad4b18e63ecbf59450a9858cd09f3e64c65aca51
Ramseyer, Abe <aramsey@ilstu.edu>
2016-10-17 21:35:41 -0500

    @author Kyle Brown: created Assignment class to hold the details of a single graded assignment

---
+++ src/Assignment.java
@@ -0,0 +1,70 @@
+package edu.ilstu;
+
+public class Assignment 
+{
+	
+private int grade;
+private char letterGrade;
+private String comment;
+
+/**
+ * @return the grade
+ */
+private int getGrade() {
+	return grade;
+}
+
+/**
+ * @param grade the grade to set
+ */
+public void setGrade(int grade) {
+	this.grade = grade;
+}
+
+/**
+ * @return the letterGrade
+ */
+public char getLetterGrade() {
+	return letterGrade;
+}
+
+/**
+ * @param letterGrade the letterGrade to set
+ */
+public void setLetterGrade(char letterGrade) {
+	this.letterGrade = letterGrade;
+}
+
+/**
+ * @return the comment
+ */
+public String getComment() {
+	return comment;
+}
+
+/**
+ * @param comment the comment to set
+ */
+public void setComment(String comment) {
+	this.comment = comment;
+}
+
+/* (non-Javadoc)
+ * @see java.lang.Object#toString()
+ */
+@Override
+public String toString() {
+	if (!comment.equals(null))
+		return "For this exam the student recieved " + grade + ", for a " + letterGrade
+				+ ". There are no comments available for this exam.";
+	else
+		return "For this assignment the student recieved " + grade + ", for a " + letterGrade
+				+ ". comments for this assignment are as follows" + comment + ".";
+		
+}
+
+
+
+	
+	
+}
