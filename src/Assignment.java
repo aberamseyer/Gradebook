cbd44bcb98bdb18c465d70d4aaf34d61b4c8801e
Ramseyer, Abe <aramsey@ilstu.edu>
2016-10-17 21:46:12 -0500

    changed a method to public and created the barebones of the GradebookFactory.java class for processing data

--- src/Assignment.java
+++ src/Assignment.java
@@ -1,70 +1,76 @@
-package edu.ilstu;
+/**
+ * 
+ * Holds the details for a single graded assignment
+ * 
+ * @author Kyle Brown
+ * 
+ */
 
 public class Assignment 
 {
-	
-private int grade;
-private char letterGrade;
-private String comment;
 
-/**
- * @return the grade
- */
-private int getGrade() {
-	return grade;
-}
+	private int grade;
+	private char letterGrade;
+	private String comment;
 
-/**
- * @param grade the grade to set
- */
-public void setGrade(int grade) {
-	this.grade = grade;
-}
+	/**
+	 * @return the grade
+	 */
+	public int getGrade() {
+		return grade;
+	}
 
-/**
- * @return the letterGrade
- */
-public char getLetterGrade() {
-	return letterGrade;
-}
+	/**
+	 * @param grade the grade to set
+	 */
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
 
-/**
- * @param letterGrade the letterGrade to set
- */
-public void setLetterGrade(char letterGrade) {
-	this.letterGrade = letterGrade;
-}
+	/**
+	 * @return the letterGrade
+	 */
+	public char getLetterGrade() {
+		return letterGrade;
+	}
 
-/**
- * @return the comment
- */
-public String getComment() {
-	return comment;
-}
+	/**
+	 * @param letterGrade the letterGrade to set
+	 */
+	public void setLetterGrade(char letterGrade) {
+		this.letterGrade = letterGrade;
+	}
 
-/**
- * @param comment the comment to set
- */
-public void setComment(String comment) {
-	this.comment = comment;
-}
+	/**
+	 * @return the comment
+	 */
+	public String getComment() {
+		return comment;
+	}
 
-/* (non-Javadoc)
- * @see java.lang.Object#toString()
- */
-@Override
-public String toString() {
-	if (!comment.equals(null))
-		return "For this exam the student recieved " + grade + ", for a " + letterGrade
-				+ ". There are no comments available for this exam.";
-	else
-		return "For this assignment the student recieved " + grade + ", for a " + letterGrade
-				+ ". comments for this assignment are as follows" + comment + ".";
-		
-}
+	/**
+	 * @param comment the comment to set
+	 */
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		if (!comment.equals(null))
+			return "For this exam the student recieved " + grade + ", for a " + letterGrade
+					+ ". There are no comments available for this exam.";
+		else
+			return "For this assignment the student recieved " + grade + ", for a " + letterGrade
+					+ ". comments for this assignment are as follows" + comment + ".";
+
+	}
 
 
 
-	
-	
+
+
 }
---
+++ src/GradebookFactory.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ * Provides all the logic for reading from files, storing them into an accessible data structure, and 
+ * outputing information based on specifications
+ * 
+ * @author Michael McHugh
+ * @author Abe Ramseyer
+ * 
+ */
+import java.io.PrintWriter;
+import java.util.Scanner;
+
+public class GradebookFactory {
+	private Scanner input;
+	private PrintWriter output;
+	
+	
+}
